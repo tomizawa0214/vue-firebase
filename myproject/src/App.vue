0084f9d11055778900ee3135bdb41b14bfc0b987
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-18 15:57:57 +0900

    Navbar

---
+++ myproject/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      title:'My first Vue app, wooo :)'
+    }
+  },
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
