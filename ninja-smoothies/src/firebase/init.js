ba44ca16f9e44a0fcd3edfc2b9c42657a64cc783
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-23 21:28:24 +0900

    firestore

---
+++ ninja-smoothies/src/firebase/init.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAxSae_SYtYjkiWsgynL0QyMZD7cqed-lA",
+  authDomain: "udemy-ninja-smoothies-bd2ba.firebaseapp.com",
+  databaseURL: "https://udemy-ninja-smoothies-bd2ba.firebaseio.com",
+  projectId: "udemy-ninja-smoothies-bd2ba",
+  storageBucket: "udemy-ninja-smoothies-bd2ba.appspot.com",
+  messagingSenderId: "881344097511",
+  appId: "1:881344097511:web:92a0047a49463169e54642"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebaseApp.firestore()
