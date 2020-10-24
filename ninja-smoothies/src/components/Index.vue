aa5fc4f3d0cafd312216fb5415b9357bd01467c4
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-24 10:46:15 +0900

    firestore delete

--- ninja-smoothies/src/components/Index.vue
+++ ninja-smoothies/src/components/Index.vue
@@ -26,8 +26,12 @@ export default {
   },
   methods: {
     deleteSmoothie(id){
-      this.smoothies = this.smoothies.filter(smoothie => {
-        return smoothie.id != id
+      // delete doc from firestore
+      db.collection('smoothies').doc(id).delete()
+      .then(() => {
+        this.smoothies = this.smoothies.filter(smoothie => {
+          return smoothie.id != id
+        })
       })
     }
   },
