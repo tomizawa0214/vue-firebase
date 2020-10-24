73fd7252378f5524aaf3ab4790776a33a970836a
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-24 12:49:45 +0900

    AddSmoothie

--- ninja-smoothies/src/views/AddSmoothie.vue
+++ ninja-smoothies/src/views/AddSmoothie.vue
@@ -1,5 +1,20 @@
 <template>
-  <h2>Add smoothie</h2>
+  <div class="add-smoothie container">
+    <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+    <form @submit.prevent="AddSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie Title:</label>
+        <input type="text" name="title" v-model="title">
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient:</label>
+        <input type="text" name="add-ingredient">
+      </div>
+      <div class="field center-align">
+        <button class="btn pink">Add Smoothie</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -7,8 +22,30 @@
     name: 'AddSmoothie',
     data() {
       return {
-
+        title: null
+      }
+    },
+    methods: {
+      AddSmoothie() {
+        console.log(this.title)
       }
     }
   }
 </script>
+
+<style>
+.add-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.add-smoothie h2 {
+  font-size: 2em;
+  margin: 20px auto;
+}
+.add-smoothie .field {
+  margin: 20px auto;
+
+}
+
+</style>
