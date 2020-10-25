74ea7164f616fb2eb866bdae5bf06cc10a372ef4
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-25 19:02:12 +0900

    編集追加

--- ninja-smoothies/src/router.js
+++ ninja-smoothies/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Index from './views/Index.vue';
 import AddSmoothie from './views/AddSmoothie.vue';
+import EditSmoothie from './views/EditSmoothie.vue';
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       name: 'AddSmoothie',
       component: AddSmoothie
     },
+    {
+      path: '/edit-smoothie/:smoothie_slug',
+      name: 'EditSmoothie',
+      component: EditSmoothie
+    }
 
   ]
 });
