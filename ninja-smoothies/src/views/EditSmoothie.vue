8fdc9f9ce3059fbd66fa2777caed37a48e3ac230
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-25 20:07:26 +0900

    フォームの編集

--- ninja-smoothies/src/views/EditSmoothie.vue
+++ ninja-smoothies/src/views/EditSmoothie.vue
@@ -1,16 +1,91 @@
 <template>
-  <div class="edit-smoothie container">
-    <h2>Edit a smoothie {{ this.$route.params.smoothie_slug }}</h2>
+  <div v-if="smoothie" class="edit-smoothie container">
+    <h2>Edit {{ smoothie.title }} Smoothie</h2>
+    <form @submit.prevent="EditSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie Title:</label>
+        <input type="text" name="title" v-model="smoothie.title">
+      </div>
+      <div v-for="(ing, index) in smoothie.ingredients" :key="index" class="field">
+        <label for="ingredient">Ingredient:</label>
+        <input type="text" name="ingredient" v-model="smoothie.ingredients[index]">
+        <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient:</label>
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+      </div>
+      <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
+        <button class="btn pink">Update Smoothie</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import db from '../firebase/init.js'
+
 export default {
   name: 'EditSmoothie',
   data() {
     return {
-
+      smoothie: null,
+      another: null,
+      feedback: null
     }
+  },
+  methods: {
+    EditSmoothie() {
+
+    },
+    addIng() {
+      if (this.another) {
+        this.smoothie.ingredients.push(this.another)
+        this.another = null
+        this.feedback = null
+      } else {
+        this.feedback = '成分を追加するには値を入力してください'
+      }
+    },
+    deleteIng(ing) {
+      this.smoothie.ingredients = this.smoothie.ingredients.filter(ingredient => {
+        return ingredient != ing
+      })
+    }
+  },
+  created() {
+    let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
+    ref.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        this.smoothie = doc.data()
+        this.smoothie.id = doc.id
+      })
+    })
   }
 }
 </script>
+
+<style>
+.edit-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.edit-smoothie h2 {
+  font-size: 2em;
+  margin: 20px auto;
+}
+.edit-smoothie .field {
+  margin: 20px auto;
+  position: relative;
+}
+.edit-smoothie .delete {
+  position: absolute;
+  right: 0;
+  bottom: 16px;
+  color: #aaa;
+  font-size: 1.4em;
+  cursor: pointer;
+}
+</style>
